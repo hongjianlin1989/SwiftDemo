d75451e307f09dfa709ee16866288a53d30da45d
hongjianlin1989 <ttzb1989@yahoo.com>
2017-04-05 12:44:07 -0400

    update with unit test

--- SwiftDemo/SwiftDemoTests/SwiftDemoTests.swift
+++ SwiftDemo/SwiftDemoTests/SwiftDemoTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class SwiftDemoTests: XCTestCase {
     
+    let movieService = MovieService.init() as MovieService
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,9 +23,16 @@ class SwiftDemoTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    func testRankApi() {
+        let completionExpectation = self.expectation(description: "No success returned")
+        let movieRequest = MovieRequest.init(startRankIndex: 5, numMovies: 5)
+        self.movieService.getRankListOfMovieWithRequest(req: movieRequest) { (Arr) in
+            XCTAssertNotNil(Arr , "The result is nil")
+            completionExpectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testPerformanceExample() {
